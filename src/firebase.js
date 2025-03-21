07e55ed60c9c6d70b1785809ecc0d087fbf565f6
Eitan Fire <eitanfire@gmail.com>
2025-03-21 17:41:28 -0600

    setup hosting and ci/cd

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnbjsVJuvBV61RdxAGGuJ131RbjGbpKiY",
+  authDomain: "board-and-train.firebaseapp.com",
+  projectId: "board-and-train",
+  storageBucket: "board-and-train.firebasestorage.app",
+  messagingSenderId: "494810551613",
+  appId: "1:494810551613:web:469c94a79a931fa6dba154",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
