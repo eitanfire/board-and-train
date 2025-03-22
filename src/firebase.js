7a16217ff5410b5c30a19c1875f20a5ac3b75117
Eitan Fire <eitanfire@gmail.com>
2025-03-21 21:03:46 -0600

    secure and add inspiration

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAnbjsVJuvBV61RdxAGGuJ131RbjGbpKiY",
-  authDomain: "board-and-train.firebaseapp.com",
-  projectId: "board-and-train",
-  storageBucket: "board-and-train.firebasestorage.app",
-  messagingSenderId: "494810551613",
-  appId: "1:494810551613:web:469c94a79a931fa6dba154",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export default app;
