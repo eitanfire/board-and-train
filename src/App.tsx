2bae1c445f8f90330794e3897f4320f9fddd8ff1
Eitan Fire <eitanfire@gmail.com>
2025-03-26 22:10:28 -0600

    add Habibi font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
-import BoardAndTrainForm from './components/BoardAndTrainForm';
-import AboutUs from "./components/AboutUs";
-import Content from "./components/Content";
+// import BoardAndTrainForm from './components/BoardAndTrainForm';
+// import AboutUs from "./components/AboutUs";
+// import Content from "./components/Content";
 import HomePage from "./pages/HomePage";
+import '../App.css';
 
 export default function App() {
   return (
