c6d41c78cdea729ec600e2b7c9cb4a6da2235378
Eitan Fire <eitanfire@gmail.com>
2025-05-17 19:12:51 -0600

    delete comments

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,11 @@ import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import { Notifications } from "@mantine/notifications"; // If you're using notifications
 import { theme } from "./theme";
 import { routeTree } from "./routeTree";
-
-// Import all necessary Mantine styles
 import "@mantine/core/styles.css";
-import "@mantine/dates/styles.css"; // If using dates
-import "@mantine/notifications/styles.css"; // If using notifications
+import "@mantine/dates/styles.css";
+import "@mantine/notifications/styles.css";
 
-// Create a new router instance
 const router = createRouter({ routeTree });
-
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
